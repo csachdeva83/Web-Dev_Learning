4a82e4e5c6c175915c1051ca4ee16fed498d9e4c
Cherish <chanisha.sachdeva45@gmail.com>
2021-09-29 17:14:07 +0530

    Intro to Node.js

---
+++ Cherish/Intro to Node/index.js
@@ -0,0 +1,9 @@
+const superheroes=require('superheroes');
+
+const supervillains=require('supervillains');
+
+var mysuperhero=superheroes.random();
+
+var mysupervillain=supervillains.random();
+
+console.log(mysuperhero," vs ",mysupervillain);
