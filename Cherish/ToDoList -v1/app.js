df151e2a521b12cf71afcd0843a733a93992e1b6
Cherish <chanisha.sachdeva45@gmail.com>
2021-09-30 22:56:55 +0530

    EJS Part-2

--- Cherish/ToDoList -v1/app.js
+++ Cherish/ToDoList -v1/app.js
@@ -3,21 +3,28 @@ const bodyParser=require('body-parser');
 
 const app=express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine","ejs");
 
 app.get("/",function(req,res){
     
     var today=new Date();
-    var currentDay=today.getDay();
-    var day="";
+    
+    var options={
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    if(currentDay===6 || currentDay===0){
-        day="Weekend";
-    }else{
-        day="Weekday";
-    }
+    var day=today.toLocaleDateString("en-us",options);
+
     res.render("list",{kindofDay: day});
 });
+
+app.post("/",function(req,res){
+    console.log(req.body.newItem);
+})
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
