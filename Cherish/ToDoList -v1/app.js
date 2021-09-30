ca147b82414af4d16aabb5dddecac63f5ec86f1b
Cherish <chanisha.sachdeva45@gmail.com>
2021-10-01 05:04:46 +0530

    EJS Complete

--- Cherish/ToDoList -v1/app.js
+++ Cherish/ToDoList -v1/app.js
@@ -1,7 +1,13 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const date=require(__dirname + "/date.js");
 
 const app=express();
+
+app.use(express.static("Public"));
+
+let items=["Buy Food","Cook Food","Eat Food"];
+let workItems=[];
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -9,22 +15,25 @@ app.set("view engine","ejs");
 
 app.get("/",function(req,res){
     
-    var today=new Date();
-    
-    var options={
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+    let day=date();
 
-    var day=today.toLocaleDateString("en-us",options);
-
-    res.render("list",{kindofDay: day});
+    res.render("list",{listTitle: day,newListItems: items});
 });
 
 app.post("/",function(req,res){
-    console.log(req.body.newItem);
-})
+    let item=req.body.newItem;
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+}); 
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle: "Work List",newListItems: workItems});
+});
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
---
+++ Cherish/ToDoList -v1/views/date.js
@@ -0,0 +1,25 @@
+exports.getDate=function(){
+    let today=new Date();
+    
+    let options={
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day=today.toLocaleDateString("en-us",options);
+
+    return day; 
+}
+
+exports.getDay=function(){
+    let today=new Date();
+    
+    let options={
+        weekday: "long"
+    };
+
+    let day=today.toLocaleDateString("en-us",options);
+
+    return day;
+}
